97358b255a5de01e13abb2c078e686bc6d1d09a1
balanagendra <balanagendra.k@spritle.com>
2023-02-13 15:33:45 +0530

    fix: updated product component

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,32 +1,52 @@
+import React, { Component } from "react"
 import Header from "../Header"
 import Footer from "../Footer"
 import "./index.css"
-const productsData =
-{
-    id: 1,
-    productName: "Fogg",
-    price: 249,
-    imageUrl:
-        "https://res.cloudinary.com/dxnhvq8pl/image/upload/v1656827930/movie%20app%20mini%20project/NativebaseLogo_zo5r6r.png",
-}
+
+class Product extends Component {
+    state = {
+        product: {}
+    }
+
+    componentDidMount() {
+        this.getProduct()
+        console.log("componsent did mount function")
+    }
+
+    getTheProductId = () => {
+        // const { id } = this.props
+        // console.log("match > ", this.props)
+        // const { params } = match
+        // const { id } = params
+        // return id
+    }
+
+    getProduct = async () => {
+        // this.getTheProductId()
+        // const productUrl = "http://localhost:3001/products/"
+    }
 
 
-const Product = () => {
-    return (
-        <div className="container-website">
-            <Header />
-            <div className="container-product">
-                <img src={productsData.imageUrl} alt={productsData.productName} />
-                <div className="product-content">
-                    <h1 className="product-name">{productsData.productName}</h1>
-                    <button className="add-to-cart-button">Add To Cart</button>
-                    <p className="product-price">RS.{productsData.price}/-</p>
-                    <button type="button">Buy Now</button>
-                </div>
+
+
+    render() {
+        console.log("render function")
+        return (
+            <div className="container-website">
+                <Header />
+                {/* <div className="container-product">
+                    <img src={productsData.imageUrl} alt={productsData.productName} />
+                    <div className="product-content">
+                        <h1 className="product-name">{productsData.productName}</h1>
+                        <button className="add-to-cart-button">Add To Cart</button>
+                        <p className="product-price">RS.{productsData.price}/-</p>
+                        <button type="button">Buy Now</button>
+                    </div>
+                </div> */}
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    )
+        )
+    }
 }
 
 export default Product
