4a3b11fb25136b6e6b1bc4ebb9a1253b12fb5c61
balanagendra <balanagendra.k@spritle.com>
2023-02-13 15:39:43 +0530

    feat: Added routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
 import Products from "./components/Products"
 import Product from './components/Product';
+import Home from "./components/Home"
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<Products />} />
         <Route exact path="/products/:productId" element={<Product />} />
       </Routes>
-    </BrowserRouter >
+    </ BrowserRouter>
   );
 }
 
